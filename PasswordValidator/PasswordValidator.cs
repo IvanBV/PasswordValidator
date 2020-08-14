c78d58e06a7f36fd8ea4167246aad0dfad5633d2
Ivan <iv.bondare@gmail.com>
2020-08-14 18:13:35 +0300

    Initial commit

---
+++ PasswordValidator/PasswordValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PasswordValidator
+{
+    public static class PasswordValidator
+    {
+        private static string _specialCharacters = @"!@#$%\^&*\(\)+=_\-{}\[\]:;\'?<>,\.""";
+
+        public static bool ValidatePassword(string password)
+        {
+            // Check string to contain at least 1 lowercase, uppercase letter, digit
+            // and string length (from 6 to 24 characters)
+            if (!Regex.IsMatch(password, @"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(^.{6,24}$)"))
+                return false;
+            
+            // Check string for maximum of 2 repeated characters
+            if (Regex.IsMatch(password, $"([0-9a-zA-Z{_specialCharacters}])\\1{{2}}"))
+                return false;
+            
+            // Final check to valid that all characters are allowed
+            if (!Regex.IsMatch(password, $"^[0-9a-zA-Z{_specialCharacters}]+$"))
+                return false;
+            
+            return true;
+        }
+    }
+}
